9b5b33f16c36778aa28036889e4f13fa7516b95f
Alexmig24 <alexandermqc2004@hotmail.com>
2026-05-15 10:26:02 -0500

    creacion de rutas

---
+++ rutas/autor.py
@@ -0,0 +1,25 @@
+from flask import Blueprint, request, jsonify
+
+from modelos.autor import Autor
+
+from repositorios.repositorio_autor import RepositorioAutor
+
+autor_bp = Blueprint("autor", __name__, url_prefix="/autor")
+repositorio = RepositorioAutor()
+
+@autor_bp.get("/")
+def obtener_todos():
+    autores = repositorio.obtener_todos()
+    return jsonify([autor.get_diccionario() for autor in autores])
+
+@autor_bp.post("/")
+def crear():
+    datos = request.get_json()
+    if not datos or not datos.get("nombre") or not datos.get("pais"): 
+        return  jsonify({"mensaje":"Verificar los datos de entrada para verificar"})
+    autor = Autor(
+        nombre = datos["nombre"],
+        pais = datos["pais"],
+    )
+    repositorio.crear(autor)
+    return jsonify(autor.get_diccionario), 201
---
+++ rutas/paper.py
@@ -0,0 +1,26 @@
+from flask import Blueprint, request, jsonify
+
+from modelos.paper import Paper
+
+from repositorios.repositorio_paper import RepositorioPaper
+
+paper_bp = Blueprint("paper", __name__, url_prefix="/paper")
+repositorio = RepositorioPaper()
+
+@paper_bp.get("/")
+def obtener_todos():
+    papers = repositorio.obtener_todos()
+    return jsonify([paper.get_diccionario() for paper in papers])
+
+@paper_bp.post("/")
+def crear():
+    datos = request.get_json()
+    if not datos or not datos.get("titulo") or not datos.get("doi") or not datos.get("id_autor"): 
+        return  jsonify({"mensaje":"Verificar los datos de entrada para verificar"})
+    paper = Paper(
+        titulo = datos["titulo"],
+        doi = datos["doi"],
+        id_autor = datos["id_autor"] 
+    )
+    repositorio.crear(paper)
+    return jsonify(paper.get_diccionario), 201
